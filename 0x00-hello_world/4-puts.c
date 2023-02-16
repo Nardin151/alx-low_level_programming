5a84fbec56f06c2c36d20a31dff10a44fd5e0d97
Nardin Philip <nardinphilip5@gmail.com>
2023-02-16 08:06:56 -0800

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 int main(void){
-
+ // printing the string: Programming is like building a multilingual puzzle
 puts("\"Programming is like building a multilingual puzzle");
 return(0);
 
