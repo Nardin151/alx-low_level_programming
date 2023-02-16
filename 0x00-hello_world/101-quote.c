a8b6a29be0b9f7fe0dcd14a9239fc7f2360c20a3
Nardin Philip <nardinphilip5@gmail.com>
2023-02-16 12:25:10 -0800

    and that piece of art is useful - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
  *Return: 0 (Success)
  **/
 int main(void)
-{echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+{
+#!/bin/bash echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 return (1);
 }
