4e8dcff05058cc6bee5b5150484c0cacdf9d4314
Nardin Philip <nardinphilip5@gmail.com>
2023-02-16 12:18:38 -0800

    and that piece of art is useful - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * main-print:and that piece of art is useful  Dora Korpar, 2015-10-19
+ * Return: 0 (Success)**/
+int main(void)
+{
+#!/bin/bash echo"and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+return (1);
+}
