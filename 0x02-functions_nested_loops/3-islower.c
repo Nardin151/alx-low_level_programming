91c557330c2c06c036a84b4c4919dbdf4f2c6fdb
Nardin Philip <nardinphilip5@gmail.com>
2023-02-21 14:24:06 -0800

    Write a function that checks for lowercase character.
    Prototype: int _islower(int c);
    Returns 1 if c is lowercase
    Returns 0 otherwise
    FYI: The standard library provides a similar function: islower. Run man islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  * Return: 0 = Success
- *_islower: checks for lowercase character.
- * c : character
+ * _islower: checks for lowercase character.
+ * c: character
  **/
 int _islower(int c);
 int _islower(int c)
 {
-if ((c >= 97) & (c<=122))
+if ((c >= 97) & (c <= 122))
 return (1);
 else
 return (0);
