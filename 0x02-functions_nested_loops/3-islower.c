7985576a6ffa11f06c37e87184bdd75ef987c7d2
Nardin Philip <nardinphilip5@gmail.com>
2023-02-21 13:58:53 -0800

    Write a function that checks for lowercase character.
    Prototype: int _islower(int c);
    Returns 1 if c is lowercase
    Returns 0 otherwise
    FYI: The standard library provides a similar function: islower. Run man islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0 = Success
+ *_islower: checks for lowercase character.
+ * c : character
+ **/
+int _islower(int c);
+int _islower(int c)
+{
+if (c == c.IsLower)
+return (0);
+else
+return (1);
+}
