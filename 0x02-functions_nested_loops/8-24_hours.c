e35558a33c25d379a13d7e11d3e26c95b3dd8010
Nardin Philip <nardinphilip5@gmail.com>
2023-02-22 15:11:42 -0800

    new file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * jack_bauer -  prints every minute of the day 
+ * of Jack Bauer, starting from 00:00 to 23:59.
+ * Return: 0 = success
+ **/
+void jack_bauer(void)
+{
+int hours = 0;
+int minutes = 0;
+int digit = 0;
+while (hours <= 23)
+{
+while (minutes <= 59)
+{
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
+_putchar(':');
+_putchar((minutes / 10) + '0');
+_putchar((minutes % 10) + '0');
+minutes++;
+}
+hours++;
+}
+return (0);
+}
