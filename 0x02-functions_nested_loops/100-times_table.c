05c4ce100bf9135fbfeb22e711275e6c7be65497
Nardin Philip <nardinphilip5@gmail.com>
2023-02-22 11:09:06 -0800

    new file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: number
+ * Return: 0 = Success
+ **/
+void print_times_table(int n)
+{
+int counter_1 = 0;
+int counter_2 = 0;
+int first_digit = 0;
+int second_digit = 0;
+int third_digit = 0;
+int mult = 0;
+while(counter_1 <= n)
+{
+while(counter_2 <= n)
+{
+if (n > 99)
+{
+mult = (counter_1 * counter_2);
+first_digit = (mult / 100) + 48;
+putchar(first_digit);
+second_digit = (mult / 10) % 10 + 48;
+putchar(second_digit);
+third_digit = mult % 10 + 48;
+putchar(third_digit);
+}
+else if (n >=10)
+{
+putchar(' ');
+second_digit = (mult / 10) % 10 + 48;
+putchar(second_digit);
+third_digit = mult % 10 + 48;
+putchar(third_digit);
+}
+else
+{
+putchar(' ');
+putchar(' ');
+third_digit = mult % 10 + 48;
+putchar(third_digit);
+}
+if (counter_2 != counter_1)
+{
+putchar(',');
+putchar(' ');
+}
+counter_2++;
+}
+putchar('\n');
+counter_2 = 0;
+counter_1++;
+}
+}
