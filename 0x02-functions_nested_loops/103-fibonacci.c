978a66be1494b7ce3750209a6777b36f0ec9cc51
Nardin Philip <nardinphilip5@gmail.com>
2023-02-22 14:42:10 -0800

    new file

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 Success
+ **/
+int main(void)
+{
+long long  unsigned int a = 1;
+long long unsigned int b = 2:
+long long unsigned int n = 0;
+long long unsigned int sum = 0;
+printf("%lld, ", a);
+printf("%lld, ", b);
+while ( n < (4000000-2))
+{
+sum = sum a + b;
+if (n < ( 4000000-2))
+prinf("%lld, ", sum);
+else
+printf("%lld", sum);
+a = b;
+b = sum;
+}
+return (0);
+}
