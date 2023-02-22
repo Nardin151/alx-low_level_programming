93e5e63874cf9c87c9d4a88ea7e3685c051f6a70
Nardin Philip <nardinphilip5@gmail.com>
2023-02-22 11:30:24 -0800

    new

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,52 +7,39 @@
  **/
 void print_times_table(int n)
 {
-int counter_1 = 0;
-int counter_2 = 0;
-int first_digit = 0;int second_digit = 0;int third_digit = 0;int mult = 0;
-while ((counter_1 <= n) & (n != 15))
+int counter_1 = 0;int counter_2 = 0;int first_digit = 0;int second_digit = 0;
+int third_digit = 0;int mult = 0;
+while ((counter_1 <= n) & (n <= 15))
 {
 while (counter_2 <= n)
 {
 mult = counter_1 * counter_2;
 if (mult > 99)
 {
-first_digit = (mult / 100) + 48;
-putchar(first_digit);
+first_digit = (mult / 100) + 48;putchar(first_digit);
 second_digit = (mult / 10) % 10 + 48;
-putchar(second_digit);
-third_digit = mult % 10 + 48;
-putchar(third_digit);
+putchar(second_digit);third_digit = mult  % 10 + 48;putchar(third_digit);
 }
 else if (mult >= 10)
 {
 if (counter_2 != 0)
-putchar(' ');
-second_digit = (mult / 10) % 10 + 48;
-putchar(second_digit);
-third_digit = mult % 10 + 48;
-putchar(third_digit);
+putchar(' ');second_digit = (mult / 10) % 10 + 48;putchar(second_digit);
+third_digit = mult % 10 + 48;putchar(third_digit);
 }
 else
 {
 if (counter_2 != 0)
 {
-putchar(' ');
-putchar(' ');
+putchar(' ');putchar(' ');
 }
-third_digit = mult % 10 + 48;
-putchar(third_digit);
+third_digit = mult % 10 + 48;putchar(third_digit);
 }
-
 if (counter_2 != n)
 {
-putchar(',');
-putchar(' ');
+putchar(',');putchar(' ');
 }
 counter_2++;
 }
-putchar('\n');
-counter_2 = 0;
-counter_1++;
+putchar('\n');counter_2 = 0;counter_1++;
 }
 }
