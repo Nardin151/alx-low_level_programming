856ec9a737eca845ef2b952a102bff9fbd88590d
Nardin Philip <nardinphilip5@gmail.com>
2023-02-21 16:51:02 -0800

    new file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * times_table - */
+ * times_table - prints the 9 times table, starting with 0.
+ * Return: 0 = Success
+ **/
+void times_table(void)
+{
+int x = 0;
+int y = 0;
+while (x <= 9)
+{
+while (y < 9)
+{
+int m = x * y;
+_putchar(m + '0');
+putchar(',');
+putchar(' ');
+y++;
+}
+int nin = 9 * x;
+putchar(nin);
+putchar('\n');
+x++;
+}
+}
