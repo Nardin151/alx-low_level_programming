c0341f18f312349040a800db52f778950159c68e
Nardin Philip <nardinphilip5@gmail.com>
2023-02-22 08:33:01 -0800

    new

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,15 @@
  **/
 int print_last_digit(int x)
 {
+if (x > 0)
+{
 int y = (x % 10) + 48;
-putchar(y);
+_putchar(y);
 return (x % 10);
+}
+else
+int e = -x;
+int r = (e % 10) + 48;
+_putchar(r);
+return(e % 10);
 }
