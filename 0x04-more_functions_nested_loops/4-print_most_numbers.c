e1d0bcd5df69ea11b7b290b944c46e40d89d6dc8
Nardin Philip <nardinphilip5@gmail.com>
2023-02-23 03:48:54 -0800

    new

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@ void print_most_numbers(void)
 int a = 0;
 while (a <= 9)
 {
-if ((a != 2) | (a != 4))
+if ((a != 2) & (a != 4))
 {
 _putchar(a + '0');
 }
-_putchar(10);
 a++;
 }
+_putchar(10);
 }
