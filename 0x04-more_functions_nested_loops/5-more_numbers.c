530c9efd76d405771510a13e3f4261de99c01a61
Nardin Philip <nardinphilip5@gmail.com>
2023-02-23 03:59:54 -0800

    changed code logic

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,3 +5,19 @@
  * 4, followed by a new line.
  * Return: 0 = success
  **/
+void more_numbers(void)
+{
+int a = 0;
+int c = 0;
+while (a <= 9)
+{
+while (c <= 14)
+{
+_putchar(c + '0');
+c++;
+}
+_putchar(10);
+c = 0;
+a++;
+}
+}
