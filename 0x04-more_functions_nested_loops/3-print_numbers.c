1ba4f1f4ff82b0d52a0dbe9117d3a4373c88bb74
Nardin Philip <nardinphilip5@gmail.com>
2023-02-23 03:15:27 -0800

    create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9, 
+ * followed by a new line
+ * Return: 0 = success
+ **/
+void print_numbers(void)
+{
+int a = 0;
+while (a <= 9)
+{
+_putchar(a + '0');
+a++;
+}
+_putchar(10);
+}
