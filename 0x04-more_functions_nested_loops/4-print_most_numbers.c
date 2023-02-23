75c1b45953e176dc39eeeaa8766a62c9f0658343
Nardin Philip <nardinphilip5@gmail.com>
2023-02-23 03:33:17 -0800

    remove whitespaces

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_most_numbers -  prints the numbers, from 0 to 9, 
+ * print_most_numbers - prints the numbers, from 0 to 9,
  * followed by a new line.
  * Return: 0 = success
  **/
 void print_most_numbers(void)
 {
 int a = 0;
-while ( a <= 9)
+while (a <= 9)
 {
 if ((a != 2) | (a != 4))
 {
