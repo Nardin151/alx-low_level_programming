a4d539089077af7d7ce70b974d9dcf1e082cb680
Nardin Philip <nardinphilip5@gmail.com>
2023-02-23 05:13:00 -0800

    creat

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * void print_diagonal -  draws a diagonal line on the terminal.
+ * @n: number of diag lines
+ **/
+void print_diagonal(int n)
+{
+int a = 0;
+int b = 0;
+while (b < n)
+{
+while (a <= b - 1)
+{
+_putchar(' ');
+a++;
+}
+a = 0;
+_putchar('\\');
+_putchar(10);
+b++;
+}
+}
