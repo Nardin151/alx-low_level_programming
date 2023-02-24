c639a57f911ce65cc83521e212e9eb78947857b3
Nardin Philip <nardinphilip5@gmail.com>
2023-02-24 02:34:55 -0800

    create 10-print_triangle.c containging a functions that that prints a triangle, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include <stdiio.h>
+#include "main.h"
+/**
+ * print_triangle - a function that prints a triangle,
+ * followed by a new line.
+ * @size: the height of the triangle
+ **/
+void print_triangle(int size)
+{
+int rows = 1;
+int whitespace = 1;
+int hash = 1;
+while (rows <= size)
+{
+while (whitespace <= size - rows)
+{
+_putchar(' ');
+whitespace++;
+}
+whitespace = 1;
+while (hash <= rows)
+{
+_putchar('#');
+hash++;
+}
+hash = 1;
+rows++;
+_putchar('\n');
+}
+if (size < rows)
+_putchar(' ');
+}
