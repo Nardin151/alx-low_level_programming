55d7f30a96c21d6442ada1607c2fcb961310a497
Nardin Philip <nardinphilip5@gmail.com>
2023-02-27 15:02:41 -0800

    change the logic

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,15 @@
 void print_array(int *a, int n)
 {
 int counter;
-for (counter = 0 ; counter < n - 1 ; counter++)
+if ((sizeof(a) / 4) > 1)
+{
+for (counter = 0 ; counter < (sizeof(a) / 4) - 1 ; counter++)
 {
 printf("%d, ", a[counter]);
 }
 printf("%d\n", a[n - 1]);
+}
+else if ((sizeof(a) / 4) == 1)
+printf("%d\n", arr[1]);
+
 }
