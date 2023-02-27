d82c849af5468096627c6da28db88a150672cc20
Nardin Philip <nardinphilip5@gmail.com>
2023-02-27 11:52:12 -0800

    change the condition

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,7 +14,7 @@ for (counter = 0 ; s[counter] != '\0' ; counter++)
 {
 length++;
 }
-for (counter_2 = length - 1 ; length != 0 ; counter_2--)
+for (counter_2 = length - 1 ; counter_2 >= 0 ; counter_2--)
 {
 _putchar(s[counter_2]);
 }
