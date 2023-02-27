63708a99bf67425a36795934f5b6477c229eaa76
Nardin Philip <nardinphilip5@gmail.com>
2023-02-27 06:52:24 -0800

    add description for int n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as
  * parameter and updates the value it points to to 98
+ * @n : a number of type int
  **/
 void reset_to_98(int *n)
 {
