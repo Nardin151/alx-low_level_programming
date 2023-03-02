99fff80bf1c00d35dab117a6402ce17ebafc4c05
Nardin Philip <nardinphilip5@gmail.com>
2023-03-02 06:38:26 -0800

    a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * cap_string - a function that capitalizes all words of a string.
+ * @x: string of chars
+ * Return: the string of chars in uppercase
+ **/
+char *cap_string(char *x)
+{
+int counter;
+int char_to_ascii;
+char ascii_to_char;
+for (counter = 0; x[counter] != '\0' ; counter++)
+{
+char_to_ascii = (int) x[counter];
+if ((char_to_ascii >= 97) & (char_to_ascii <= 122))
+{
+char_to_ascii -= 32;
+ascii_to_char = (char) char_to_ascii;
+x[counter] = ascii_to_char;
+}
+}
+return (x);
+}
