72f0f8f4f550166f15c5c1f85cfe93976da6c556
Nardin Philip <nardinphilip5@gmail.com>
2023-03-03 02:03:41 -0800

    Add tabulation char in the s array

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,15 @@ char *cap_string(char *x)
 int count;
 int char_to_ascii;
 char ascii_to_char;
-int s[12] = {'\n', ' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int s[13] = {'\n', ' ', ',', ';', '.'
+, '!', '?', '"', '(', ')', '{', '}', '\t'};
 int count2;
 for (count = 0; x[count] != '\0' ; count++)
 {
 char_to_ascii = (int) x[count];
 if ((char_to_ascii >= 97) & (char_to_ascii <= 122))
 {
-for (count2 = 0 ; count2 < 12 ; count2++)
+for (count2 = 0 ; count2 < 13 ; count2++)
 {
 if (((int)x[count - 1] == s[count2]) & (count != 0))
 {
