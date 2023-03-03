12c275a407723907d267d71529feeaf908091353
Nardin Philip <nardinphilip5@gmail.com>
2023-03-03 01:32:36 -0800

    Add Space after ,

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,8 +8,8 @@
 char *leet(char *x)
 {
 int count;
-char Alpha[5] = {'A','E','O','T','L'};
-int leet_code[5] = {4,3,0,7,1};
+char Alpha[5] = {'A', 'E', 'O', 'T', 'L'};
+int leet_code[5] = {4, 3, 0, 7, 1};
 int count2;
 for (count = 0 ; x[count] != '\0' ; count++)
 {
@@ -17,5 +17,5 @@ for (count2 = 0 ; count2 < 5 ; count2++)
 if ((x[count] == Alpha[count2]) || ((char) (x[count] - 32) == Alpha[count2]))
 x[count] = (char) leet_code[count2] + 48;
 }
-return (x); 
+return (x);
 }
