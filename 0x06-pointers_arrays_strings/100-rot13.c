ef454b581cd0bc2792a6ec67aaf7b6c8f753f325
Nardin Philip <nardinphilip5@gmail.com>
2023-03-03 01:58:02 -0800

    a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rot13 -  function that encodes a string using rot13.
+ * @x: string of chars
+ * Return: a string of chars encoded into rot13
+ **/
+char *rot13(char *x)
+{
+int counter;
+char ALPHA[26] = {'A','B','C','D','E','F','G','H','I','J','K','L',
+'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+char alpha[26] = 
+for (counter = 0 ; x[counter] != '\0' ; counter++)
+{
+
+}
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,15 +10,22 @@ char *cap_string(char *x)
 int count;
 int char_to_ascii;
 char ascii_to_char;
+int s[12] = {'\n', ' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+int count2;
 for (count = 0; x[count] != '\0' ; count++)
 {
 char_to_ascii = (int) x[count];
 if ((char_to_ascii >= 97) & (char_to_ascii <= 122))
 {
-if ((((int)x[count - 1] == 32) || ((int) x[count - 1] == 10)) & (count != 0))
+for (count2 = 0 ; count2 < 12 ; count2++)
+{
+if (((int)x[count - 1] == s[count2]) & (count != 0))
+{
 char_to_ascii -= 32;
 ascii_to_char = (char) char_to_ascii;
 x[count] = ascii_to_char;
+}
+}
 }
 }
 return (x);
