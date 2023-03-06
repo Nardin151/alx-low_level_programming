ced6d1afe73deedf283cd314f3847f6e8eb10433
Nardin Philip <nardinphilip5@gmail.com>
2023-03-06 15:13:44 -0800

    a function that prints the chessboard.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,7 +11,13 @@ int counter2;
 for (counter = 0 ; counter < 8 ; counter++)
 {
 for (counter2 = 0; counter2 < 8 ;counter2++)
-_putchar(a[counter][counetr2]);
+{if (a[counter][counter2] == ' ')
+{
+break;
+}
+else
+_putchar(a[counter][counter2]);
+}
 _putchar('\n');
 }
 }
