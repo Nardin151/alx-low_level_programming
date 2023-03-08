473c5e33673c0dc1f690e8d2bfc13fba59c4d9c0
Nardin Philip <nardinphilip5@gmail.com>
2023-03-08 05:50:30 -0800

    a function that returns the length of a string.

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: string of chars
+ **/
+void _print_rev_recursion(char *s)
+{
+
+}
