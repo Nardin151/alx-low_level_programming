65fc058fb0839a8f7028dc0f326e7d46fc33317e
Nardin Philip <nardinphilip5@gmail.com>
2023-03-10 06:38:56 -0800

    Add __attribute__((unused))

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: the value of the
  * Return: 0 = success
  **/
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 printf("%d\n", argc);
 return (0);
