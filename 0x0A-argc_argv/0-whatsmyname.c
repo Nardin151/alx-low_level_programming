c59a06412c94075172af563a06856233b497586d
Nardin Philip <nardinphilip5@gmail.com>
2023-03-10 06:28:33 -0800

    Use __attribute__((unused)) with unused variables or parameters

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: the value of each input as a string of chars
  * Return: 0 = success
  **/
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
