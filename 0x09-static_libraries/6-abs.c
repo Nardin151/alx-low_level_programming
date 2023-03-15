84863ec046ee9923260c02e882689deeb2959554
Nardin Philip <nardinphilip5@gmail.com>
2023-03-15 09:59:55 -0700

    function that returns the abs value of an int

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - gets tha absolute value of a number
+ * @n: int
+ * Return: positive int
+ **/
+int _abs(int n)
+{
+if (n < 0)
+return (n * -1)
+return (n);
+}
