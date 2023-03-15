69da26c867e4e5333b09610df831ed2070c46cdf
Nardin Philip <nardinphilip5@gmail.com>
2023-03-15 09:02:50 -0700

    function copies n bytes * from memory area src to memory area dest

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memcpy - function copies n bytes from memory area src to memory area dest
+ * @dest: string of char that we will copy to
+ * @n: the number of chars to copy
+ * @src: string of char that we will copy from
+ * Return: dest after copying
+ **/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int counter;
+for (counter = 0 ; counter < n ; counter++)
+dest[counter] = s[counter];
+return (dest);
+}
