71a8a08640a499fbe7be641a82b7154f87d955ee
Nardin Philip <nardinphilip5@gmail.com>
2023-03-22 12:01:15 -0700

    a function that searches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int counter;
+if (size < 0)
+return (-1);
+for (counter = 0 ; counter < size ; counter++)
+{
+if (cmp(array[counter]) != 0)
+return (counter);
+}
+return (-1);
+}
