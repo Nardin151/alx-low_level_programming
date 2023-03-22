82b7bffb725fdb5fbce27f509af03fffe6f572f8
Nardin Philip <nardinphilip5@gmail.com>
2023-03-22 08:58:58 -0700

    a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,22 +9,28 @@
 int is_palindrome(char *s)
 {
 int counter;
-int *end;
-int target;
 int size = 0;
 for (counter  = 0; s[counter] ; counter++)
 size++;
 return (Palindorm(0, size - 1, s));
 }
+/**
+ * Palindorm -  function that returns 1 if a string if
+ * a palindrome and 0 if not
+ * @s: string of char
+ * @start: 0
+ * @end: length -1
+ * Return: 1 if a string is a palindrome and 0 if not
+ **/
 int Palindorm(int start, int end, char *s)
 {
-if (s[start] == s[end] & start == end )
+if ((s[start] == s[end]) & (start == end))
 return (1);
-if (s[start] == s[end] & start == end - 1)
+if ((s[start] == s[end]) & (start == end - 1))
 return (1);
 if (s[start] != s[end])
 return (0);
 if (s[start] == s[end])
-return (Palindorm(start + 1 , end - 1, s));
+return (Palindorm(start + 1, end - 1, s));
 return (0);
 }
