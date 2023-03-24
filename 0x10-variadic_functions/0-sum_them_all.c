d955d4999ee0e56556c743e33c17c1aac30238e6
Nardin Philip <nardinphilip5@gmail.com>
2023-03-24 04:29:56 -0700

    Creat a function that sums all the numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - sum undefined number of arg
+ * @n: fixed argument = number of arg
+ * Return: sum of all the numbers
+ **/
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i = 0, sum = 0;
+va_list ap;
+if (n == 0)
+return (0);
+va_start(ap, n);
+while (i < n)
+{
+sum += va_arg(ap, int);
+i++;
+}
+va_end(ap);
+return (sum);
+}
