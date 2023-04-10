81dcfe1a390c41f86d4f374392020b22f3e3ded1
Nardin Philip <nardinphilip5@gmail.com>
2023-04-10 09:55:26 -0700

    Add parantheses around n & one

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 unsigned long int one = 1 << index;
 if (n & one)
 return (1);
-else if (n & one == 0)
+else if ((n & one) == 0)
 return (0);
 return (-1);
 }
