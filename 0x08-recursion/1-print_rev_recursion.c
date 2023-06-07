42a6234220b1b47d5a38b2431f647b4ccd04b888
Nardin Philip <nardinphilip5@gmail.com>
2023-06-07 16:32:50 -0700

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * Len - get the length of a string
+ * @s: String
+ * Return: Length of a string
+ **/
+int Len(char *s)
+{
+if (s[0] == '\0')
+return (0);
+else
+{
+s++;
+return (1 + Len(s));
+}
+}
+/**
+ * rev - print a string in reverse
+ * @s: the String that we want to print in reverse
+ * @n: Length of S
+ **/
+void rev(char *s, int n)
+{
+if (n == 0)
+{
+_putchar(s[0]);
+}
+else
+{
+_putchar(s[n]);
+rev(s, n - 1);
+}
+}
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: the String that we want to print in reverse
+ **/
+void _print_rev_recursion(char *s)
+{
+rev(s, Len(s));
+}
