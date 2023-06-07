57cc235d5f5d3ed75f1467dd99abee7cc023ba7f
Nardin Philip <nardinphilip5@gmail.com>
2023-06-07 14:48:03 -0700

    Change the placement of the 2 functions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,5 @@
 #include "main.h"
 /**
- * is_prime_number - a function that returns 1 if the input
- * integer is a prime number, otherwise return 0.
- * @n: Number
- * Return: 1 for prime numbers and 0 for non-prime
- **/
-int is_prime_number(int n)
-{
-return (Prime(n, 2));
-}
-/**
  * Prime - finds if a number is prime or not
  * @n: Number
  * @m: Counter
@@ -27,4 +17,14 @@ else if (n % m == 0)
 return (0);
 else
 return (Prime(n, m + 1));
+}
+/**
+ * is_prime_number - a function that returns 1 if the input
+ * integer is a prime number, otherwise return 0.
+ * @n: Number
+ * Return: 1 for prime numbers and 0 for non-prime
+ **/
+int is_prime_number(int n)
+{
+return (Prime(n, 2));
 }
