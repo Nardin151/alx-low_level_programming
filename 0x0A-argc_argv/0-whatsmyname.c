2bba869b0c8858203d7138c845e44f9b8d28d2a9
Nardin Philip <nardinphilip5@gmail.com>
2023-06-15 08:23:24 -0700

    Add white space after ',' at line 9 and descriped the var 'argc' and 'argv' in the comments correclty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - a program that prints its name, followed by a new line.
- * @argc: array of all the arguemnts as strings
+ * @argc: the number of arguemnts
+ * @argv: array of all the arguemnts as strings
  * Return: 0 on success
  **/
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-printf("%s\n",argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
